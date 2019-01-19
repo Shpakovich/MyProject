52f9c00cd6e2b1bf318fd7be3ee3a9cfda5a6141
Denis Shmakov <shmakovbx@gmail.com>
2019-01-19 23:25:06 +0300

    CONDITION Friday of 13

--- Calendar/calendar.js
+++ Calendar/calendar.js
@@ -16,7 +16,7 @@ result=0;
 		// убираем подпись под днем недели при первом запуске
 		var ps = document.querySelector(".ps");
 		ps.style.display = "none"; 
-		//
+		// присваеваем значение инпутов в переменные 
 		day = document.getElementById('day').value;
 		month = document.getElementById('month').value;
 		//разделение года на 2 части
@@ -26,6 +26,7 @@ result=0;
 		numYear = b[2] + b[3];
 		numCentury = b[0] + b[1];
 
+		// меняем значения месяцев и года формулы на нормальные
 		if (+month >= 3){
 			month = +month - 2;
 		}
@@ -36,7 +37,7 @@ result=0;
 		if (month > 10) {
 			numYear = numYear - 1;
 		}
-		
+		// формула рассчёта дня недели
 		W = +day + Math.floor((13 * +month - 1)/5) + +numYear + Math.floor(+numYear / 4) + Math.floor(+numCentury / 4) - 2 * +numCentury;
 
 		result = Math.abs(W)%7;
@@ -44,7 +45,7 @@ result=0;
 		if(W < 7){
 			result = W;
 		}
-
+		//вывод на экран(замена дифа)
 	switch (result)
 		{
 		case 0:	
@@ -67,6 +68,11 @@ result=0;
 		if(month>10 || day>31){
 			weekp.innerHTML = "Error type of Date";
 		}
+
+		if( day == 13 && result == 6){
+			alert("uuuuuu")
+		}
+
 }
 
 
